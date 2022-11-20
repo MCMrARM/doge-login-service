c541dec48caa2916d9eb0a3157ec89306a69fbf2
MCMrARM <mcmrarm@gmail.com>
2022-11-20 14:45:45 +0100

    Add config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+import fs from "fs";
+export const config = {
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: "https://discord.mrarm.io/auth/discord",
+    authCookieKey: Buffer.from(process.env.AUTH_COOKIE_KEY, "base64"),
+    jwtPrivateKey: process.env.JWT_PRIVATE_KEY
+};
+
