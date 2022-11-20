b6ebb8bd9d1c03b66780871e9d4cb539f4e0cec7
MCMrARM <mcmrarm@gmail.com>
2022-11-20 14:58:28 +0100

    Fix config types

--- config.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-export const config = {
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: "https://discord.mrarm.io/auth/discord",
-    authCookieKey: Buffer.from(process.env.AUTH_COOKIE_KEY, "base64"),
-    jwtPrivateKey: process.env.JWT_PRIVATE_KEY
-};
-
