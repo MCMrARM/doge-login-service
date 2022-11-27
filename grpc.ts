57ea9a50d163e70d0fc5a9fed6f3d6a082d9f83b
MCMrARM <mcmrarm@gmail.com>
2022-11-28 00:26:40 +0100

    startGrpcServer: bind => bindAsync

--- grpc.ts
+++ grpc.ts
@@ -3,7 +3,12 @@ import {NodeRegistryService} from "./rpc/services/NodeRegistryService.js";
 
 export function startGrpcServer() {
     let server = new grpc.Server();
-    server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
-    new NodeRegistryService(server);
-    server.start();
+    server.bindAsync("127.0.0.1:50051", grpc.ServerCredentials.createInsecure(), (err, cb) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        new NodeRegistryService(server);
+        server.start();
+    });
 }
